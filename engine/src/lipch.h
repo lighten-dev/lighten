ebc0ce6ec8044ae664b3d0547ad62ad0f10707fe
malatindez <53783400+malatindez@users.noreply.github.com>
2022-04-11 11:14:58 +0300

    add precompiled header file

---
+++ engine/src/lipch.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+#ifndef SPDLOG_ACTIVE_LEVEL
+    #if _DEBUG
+        #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+    #else
+        #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_ERROR
+    #endif
+#endif
+
+#include <spdlog/spdlog.h>
